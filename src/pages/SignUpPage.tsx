6a77a05d57d7c0f9d22e19dfa407806670b20a98
Khakimov Mukhammadaziz <khakimovmukhammadaziz@gmail.com>
2025-08-07 22:56:39 +0500

    steps updated

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,31 +1,38 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Step1 from "../components/steps/step-1";
+import Step2 from "../components/steps/step-2";
+import Step3 from "../components/steps/step-3";
+import Step4 from "../components/steps/step-4";
 import Button from "../components/ui/Button";
 import Icon from "../components/ui/Icon";
-import Input from "../components/ui/Input";
-import ProgressStepAuth from "../components/ui/progress-step-auth";
+import ProgressStep from "../components/ui/progress-step-auth";
 import useStepProgressAuth from "../hooks/useStepProgressAuth";
-import Step1 from "../components/steps/step-1";
 
 const SignUpPage = () => {
-  const [currentStep, setCurrentStep] = useState(1);
-  const [isSuccess, setIsSuccess] = useState(false);
   const totalStep = 4;
+  const [currentStep, setCurrentStep] = useState(1);
+  const [isSuccess, setisSuccess] = useState(false);
   const { progressData, setProgressData } = useStepProgressAuth();
-  const handleSavePreviusStep = () => {
-    const findStep = progressData.find((step) => step.step === currentStep - 1);
-    findStep.isSuccess = true;
-    setProgressData([...progressData]);
-  };
   const incrementCurrentStep = () => {
-    if (currentStep <= 4) {
-      setCurrentStep((prevState) => prevState + 1);
+    if (currentStep < totalStep) {
+      const updatedSteps = progressData.map((step) =>
+        step.step === currentStep ? { ...step, isSuccess: true } : step
+      );
+      setProgressData(updatedSteps);
+      setCurrentStep((prev) => prev + 1);
     }
   };
-  useEffect(() => {
-    if (currentStep !== 1) {
-      handleSavePreviusStep();
+
+  const decrementCurrentStep = () => {
+    if (currentStep > 1) {
+      const updatedSteps = progressData.map((step) =>
+        step.step === currentStep - 1 ? { ...step, isSuccess: false } : step
+      );
+      setProgressData(updatedSteps);
+      setCurrentStep((prev) => prev - 1);
     }
-  }, [currentStep]);
+  };
   return (
     <section className="h-screen p-[20px_35px_30px_35px] bg-[#F4F9FD]">
       <div className="flex h-full gap-x-8">
@@ -37,30 +44,52 @@ const SignUpPage = () => {
             <p className="description text-white text-[40px] max-w-[400px]">
               Get started
             </p>
-            <ProgressStepAuth steps={progressData} currentStep={currentStep} />
+            <ProgressStep steps={progressData} currentStep={currentStep} />
           </div>
         </div>
-        <div className="w-[100%] flex flex-col justify-between max-w-[70%] rounded-[24px]  bg-white shadow-[0px_6px_rgba(196_203_214_0.5)]">
-          <div className="flex flex-col max-w-[403px] w-full mx-auto items-center pt-[75px]">
-            <span className="text-[14px] text-[#3F8CFF] font-[700]">
+        <div className="w-[100%] flex flex-col justify-between pb-[18px] max-w-[70%] rounded-[24px] bg-white shadow-[0px_6px_rgba(196_203_214_0.5)]">
+          <div className="flex flex-col max-w-[403px] mx-auto items-center pt-[75px]">
+            <span className="font-bold text-[14px] text-[#3F8CFF]">
               Step {currentStep}/{totalStep}
             </span>
-            <h2 className="signin-title">
+            <h2 className="signin-title text-[22px] font-bold">
               {progressData[currentStep - 1].title}
             </h2>
-            <Step1 />
+            {currentStep === 1 && <Step1 />}
+            {currentStep === 2 && <Step2 />}
+            {currentStep === 3 && <Step3 />}
+            {currentStep === 4 && <Step4 />}
           </div>
-
-          <div className="border-t-2 border-[#E4E6E8] py-[18px]">
-            <Button
-              disabled={isSuccess}
-              variant="small"
-              className={`flex items-center gap-x-3 ml-auto mr-10 ${
-                !isSuccess && "disabled"
-              }`}
-            >
-              Next Step <Icon.rightArrowIcon />
-            </Button>
+          <div className="border-t-2 border-[#E4E6EF] pt-[18px] mt-10 flex items-center">
+            {currentStep > 1 && (
+              <button
+                onClick={decrementCurrentStep}
+                className="flex items-center gap-[5px] border-none text-[#3F8CFF] ml-[56px]"
+              >
+                <Icon.previusIcon />
+                Previous
+              </button>
+            )}
+            {currentStep === 4 ? (
+              <Link
+                to={"/successfull"}
+                className="link flex gap-x-3 items-center ml-auto mr-10"
+              >
+                Next Step <Icon.rightArrowIcon />
+              </Link>
+            ) : (
+              <Button
+                onClick={incrementCurrentStep}
+                disabled={false}
+                variant="small"
+                className={`flex gap-x-3 items-center ml-auto mr-10 ${
+                  !isSuccess && "disabled"
+                }`}
+              >
+                Next Step
+                <Icon.rightArrowIcon />
+              </Button>
+            )}
           </div>
         </div>
       </div>
